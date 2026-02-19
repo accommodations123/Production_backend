20ed8a919d55854ac3a6d4fa8941ea4ed06809d4
Bhargav1706 <bhargavreddy.mettu@gmail.com>
2026-02-19 11:34:29 +0530

    jenkins

--- routes/otp.routes.js
+++ routes/otp.routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import {sendOTP,verifyOTP,logout } from '../controllers/otp.controller.js'
+import {sendOTP,verifyOTP,logout,updateUser } from '../controllers/otp.controller.js'
 import {rateLimit} from '../middleware/rateLimiter.js'
 import userAuth from '../middleware/userAuth.js';
 import { auditContext } from "../middleware/auditContext.js";
+import { upload } from "../middleware/upload.js"; 
 const router = express.Router()
 router.post('/send-otp',auditContext("public"),rateLimit,sendOTP)
 router.post('/verify-otp',auditContext("public"),rateLimit,verifyOTP)
